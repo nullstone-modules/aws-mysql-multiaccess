7c36b7be87c9a591fba581a182ede7b559087ee9
Brad Sickles <brad.sickles@gmail.com>
2025-03-25 16:16:21 -0400

    Added interpolation to additional database names

--- variables.tf
+++ variables.tf
@@ -44,7 +44,32 @@ variable "additional_database_names" {
   description = <<EOF
 Additional databases to create in the mysql cluster. If any already exist, they will be reused.
 For each database, a new user will be created and granted owner permissions to the database schema.
+
+The following identifiers are supported for interpolation:
+  {{ NULLSTONE_STACK }}
+  {{ NULLSTONE_BLOCK }}
+  {{ NULLSTONE_ENV }}
 EOF
   default     = []
+}
+
+locals {
+  clean_additional_database_names = [for db in var.additional_database_names : db if trim(coalesce(db, "")) != ""]
+}
+
+data "ns_env_variables" "additional_db_names" {
+  for_each = local.clean_additional_database_names
+
+  input_env_variables = tomap({
+    NULLSTONE_STACK = local.stack_name
+    NULLSTONE_APP   = local.block_name
+    NULLSTONE_ENV   = local.env_name
+    DATABASE_NAME   = each.value
+  })
+  input_secrets = tomap({})
+}
+
+locals {
+  additional_database_names = toset([for ev in data.ns_env_variables.additional_db_names : ev.env_variables["DATABASE_NAME"]])
 }
 
